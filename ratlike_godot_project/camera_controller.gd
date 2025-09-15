58901f4670021d3a2d98ad7a1030a60ec098e858
Thomas Nguyen <GODCASTERTAM919@GMAIL.COM>
2025-09-15 17:38:13 -0400

    Camera control
    Added camera node objects and a simple springarm. Camera rotates based on mouse. Player movement is still based on x, z axis.

---
+++ ratlike_godot_project/camera_controller.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+@onready var yaw_node = $CamYaw
+@onready var pitch_node = $CamYaw/CamPitch
+@onready var camera = $CamYaw/CamPitch/Camera3D
+
+var yaw : float = 0
+var pitch : float = 0
+
+var yaw_sensitivity : float = 0.07
+var pitch_sensitivity : float = 0.07
+
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+
+var pitch_max : float = 75
+var pitch_min : float = -55
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += event.relative.y * pitch_sensitivity
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+func _physics_process(delta):
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	
+	#yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	#pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
+	
+	yaw_node.rotation_degrees.y = yaw
+	pitch_node.rotation_degrees.x = pitch
