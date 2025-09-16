de00000b2a5e854e16ab5fda7da3b86c95b1e1db
John Randall <john@rand4.us>
2025-09-16 13:31:07 -0400

    Added sound when the player collects cheese

--- ratlike_godot_project/cheese.gd
+++ ratlike_godot_project/cheese.gd
@@ -2,8 +2,14 @@ extends Area3D
 
 signal cheese_collected
 
+@onready var sound = $AudioStreamPlayer3D
+
 func _on_body_entered(body):
+	sound.play()
+	$"Cheese Wheel".visible = false
+
+
+func _on_audio_stream_player_3d_finished() -> void:
 	print("CHEESE TOUCH")
 	emit_signal("cheese_collected")
 	queue_free()
-	return
