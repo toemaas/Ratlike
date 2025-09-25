4b869364571d232b2a049b585a0f0b0f9169aa6f
Thomas Nguyen <GODCASTERTAM919@GMAIL.COM>
2025-09-25 12:20:46 -0400

    Added pause menu with resume/quit buttons

---
+++ ratlike_godot_project/main.gd
@@ -0,0 +1,20 @@
+# main.gd
+extends Node3D
+
+@onready var pause_menu = $PauseMenu
+
+func _ready() -> void:
+	pause_menu.hide()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_pause"):
+		get_tree().root.set_input_as_handled()
+		toggle_pause() # Call the new function
+
+func toggle_pause():
+	get_tree().paused = not get_tree().paused
+	if get_tree().paused:
+		pause_menu.show()
+	else:
+		pause_menu.hide()
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
---
+++ ratlike_godot_project/pause_menu.gd
@@ -0,0 +1,16 @@
+# pause_menu.gd
+extends Control
+
+func _on_resume_pressed() -> void:
+	# We don't need hide() or get_tree().paused here anymore.
+	# The parent's function handles everything.
+	get_parent().toggle_pause()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_pause"):
+		get_tree().root.set_input_as_handled()
+		get_parent().toggle_pause()
+		
